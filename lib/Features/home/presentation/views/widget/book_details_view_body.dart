d29f9d630db17d98010399668be3509e25e0eb86
eoYe8 <alfee38l@gmail.com>
2025-05-30 17:18:54 +0300

    add title and subtitle to details view

--- lib/Features/home/presentation/views/widget/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widget/book_details_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:free_book_shelf/Features/home/presentation/views/widget/custom_book_detail_app_Bar.dart';
 import 'package:free_book_shelf/Features/home/presentation/views/widget/custom_book_image.dart';
+import 'package:free_book_shelf/core/utils/styles.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
@@ -13,9 +14,19 @@ class BookDetailsViewBody extends StatelessWidget {
       child:  Column(children: [
         CustomBookDetailsAppBar(),
         Padding(
-          padding: EdgeInsets.symmetric(horizontal: width * .15),
+          padding: EdgeInsets.symmetric(horizontal: width * .2),
           child: CustomBookImage(),
         ),
+        SizedBox(height: 43,),
+        Text('The Best Gifts',style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),),
+         SizedBox(height: 6,),
+        Opacity(
+          opacity: .7,
+          child: Text('The Jungle book',style: Styles.textStyle18.copyWith(
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.w500,
+          ),),
+        )
         ]),
     );
   }
