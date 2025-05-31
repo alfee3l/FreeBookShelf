e1939ab4a7a4cc44b228b15b81e59f28a3281e1d
eoYe8 <alfee38l@gmail.com>
2025-05-31 22:34:26 +0300

    impl fetch newest books

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -3,6 +3,6 @@ import 'package:free_book_shelf/Features/home/data/models/book_model/book_model.
 import 'package:free_book_shelf/core/errors/failures.dart';
 
 abstract class HomeRepo{
- Future<Either<Failures,List<BookModel>>> fetchBestSellerBooks();
+ Future<Either<Failures,List<BookModel>>> fetchNewsetBooks();
   Future<Either<Failures,List<BookModel>>>fetchFeatureBooks();
 }
--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -2,12 +2,25 @@ import 'package:dartz/dartz.dart';
 import 'package:free_book_shelf/Features/home/data/models/book_model/book_model.dart';
 import 'package:free_book_shelf/Features/home/data/repos/home_repo.dart';
 import 'package:free_book_shelf/core/errors/failures.dart';
+import 'package:free_book_shelf/core/utils/api_service.dart';
 
-class HomeRepoImpl  extends HomeRepo {
+class HomeRepoImpl extends HomeRepo {
+  final ApiService apiService;
+  HomeRepoImpl(this.apiService);
   @override
-  Future<Either<Failures, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failures, List<BookModel>>> fetchNewsetBooks()async {
+  try {
+  var data = await apiService.get(endPoint: 'volumes?Filtering=free-ebooks&q=subject:Programming&Sorting=newest');
+   List<BookModel> books =[];
+   for (var item in data['items']) {
+    books.add(BookModel.fromJson(item));
+    return right(books);
+     
+   }
+
+}  catch (e) {
+  return left(ServerFailure());
+}
   }
 
   @override
